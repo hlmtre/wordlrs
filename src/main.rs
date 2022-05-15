a135695c57f756c45b5747b52898bd9b3eb7e27e
Matt Weller <hellmitre@gmail.com>
2022-05-15 07:51:22 -0700

    replace file with wordle valid solutions

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let perfect_letters = wordlrs::get_letter_placements(five_letters);
     // go get the words
-    let url = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words";
+    // this is the actual list of wordle answers
+    let url = "https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/28804271b5a226628d36ee831b0e36adef9cf449/wordle-answers-alphabetical.txt";
+    // this is i think the list of valid guesses... that isn't valid answers.
+    //let url = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words";
     let resp = reqwest::blocking::get(url)?;
     let status = resp.status();
     if status != reqwest::StatusCode::OK {
